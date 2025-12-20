b5c8115322bce47d29baffa62609e841c8f9d881
laurentvuillaume <figura.graphik@gmail.com>
2025-12-20 16:30:20 +0100

    add ProjectCard component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,6 @@
 // app/projets/page.tsx
-import Link from "next/link";
 import { getAllProjects } from "@/lib/actions/project.actions";
-import Image from "next/image";
-import { Card, CardContent } from "@/components/ui/card";
+import ProjectCard from "@/components/project-card";
 
 export default async function ProjetsPage() {
   const { data: projects } = await getAllProjects();
@@ -18,42 +16,7 @@ export default async function ProjetsPage() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects?.map((project) => (
-          <Link
-            href={`/projects/${project.slug}`}
-            key={project.slug}
-            className="group"
-          >
-            <div className="overflow-hidden rounded-xl border bg-card text-card-foreground shadow-sm transition-all duration-300 hover:shadow-md">
-              <div className="aspect-video bg-muted/50 flex items-center justify-center">
-                {project.image && (
-                  <Image
-                    src={project.image}
-                    alt={project.title}
-                    width={200}
-                    height={150}
-                    className="object-fill"
-                    priority
-                  />
-                )}
-              </div>
-              <div className="p-6">
-                <h3 className="text-xl font-semibold mb-2 group-hover:text-primary transition-colors">
-                  {project.title}
-                </h3>
-                <p className="text-muted-foreground mb-4">{project.excerpt}</p>
-                <div className="flex flex-wrap gap-2">
-                  {project.technologies.map((tag, i) => (
-                    <span
-                      key={i}
-                      className="text-xs px-2 py-1 bg-muted rounded-full"
-                    >
-                      {tag.name}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </Link>
+          <ProjectCard key={project.id} project={project} />
         ))}
       </div>
     </div>
---
+++ components/project-card.tsx
@@ -0,0 +1,50 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+const ProjectCard = ({
+  project,
+}: {
+  project: {
+    id: string;
+    title: string;
+    slug: string;
+    excerpt: string;
+    image: string;
+    technologies: { id: string; name: string }[];
+  };
+}) => {
+  return (
+    <Link href={`/projects/${project.slug}`} className="group">
+      <div className="overflow-hidden rounded-xl border bg-card text-card-foreground shadow-sm transition-all duration-300 hover:shadow-md">
+        <div className="aspect-video bg-muted/50 flex items-center justify-center">
+          {project.image && (
+            <Image
+              src={project.image}
+              alt={project.title}
+              width={200}
+              height={150}
+              className="object-fill"
+              priority
+            />
+          )}
+        </div>
+        <div className="p-6">
+          <h3 className="text-xl font-semibold mb-2 group-hover:text-primary transition-colors">
+            {project.title}
+          </h3>
+          <p className="text-muted-foreground mb-4">{project.excerpt}</p>
+          <div className="flex flex-wrap gap-2">
+            {project.technologies.map((tag, i) => (
+              <span key={i} className="text-xs px-2 py-1 bg-muted rounded-full">
+                {tag.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProjectCard;
