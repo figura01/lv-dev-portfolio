f57610c9efb43ea137b107e08a7c927162b8ce82
laurentvuillaume <figura.graphik@gmail.com>
2025-12-22 19:06:00 +0100

    up test

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,23 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
-import { additionalFields } from "better-auth/plugins/additional-fields";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-  plugins: [
-    inferAdditionalFields<typeof auth>(),
-    additionalFields({
-      user: {
-        fields: {
-          role: {
-            type: "string",
-            required: true,
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [inferAdditionalFields<typeof auth>()],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
--- lib/auth-config.ts
+++ lib/auth-config.ts
@@ -1,7 +1,6 @@
 // lib/auth-config.ts
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
-import { additionalFields } from "better-auth/plugins/additional-fields";
 import { env } from "process";
 const isProduction = process.env.NODE_ENV === "production";
 export const authConfig = {
@@ -27,20 +26,12 @@ export const authConfig = {
       },
     },
   },
-  session: {},
-  plugins: [
-    nextCookies(),
-    additionalFields({
-      user: {
-        fields: {
-          role: {
-            type: "string",
-            required: true,
-            default: "USER",
-          },
-        },
-      },
-    }),
-  ],
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
+  plugins: [nextCookies()],
 };
 export const auth = betterAuth(authConfig);
